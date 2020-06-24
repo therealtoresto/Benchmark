e416b82706d945a2cd71c330eb0237086350d120
TORESTO <therealtoresto@gmail.com>
2020-06-24 20:35:21 +0300

    for keys/of/in benchmarking

---
+++ 9-for-in.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const benchmark = require('./2-benchmark.js');
+
+const data = {
+    a: 'abc',
+    bcd: 'defg',
+    efgh: 'hijklmn',
+    lmnopqrs: 'u',
+    tvuwx: 'v',
+    yz: 'xyz'
+};
+
+const testForKeys = () => {
+    const a = Array(7);
+    const keys = Object.keys(data);
+    for (let i = 0; i < keys.length; i++) {
+        const key = keys[i];
+        a[i] = data[key];
+    }
+};
+
+const testForIn = () => {
+    const a = Array(7);
+    let i = 0;
+    for (const key in data) {
+        a[i++] = data[key];
+    }
+};
+
+const testForOf = () => {
+    const a = Array(7);
+    let i = 0;
+    const keys = Object.keys(data);
+    for (const key of keys) {
+        const val = data[key];
+        a[i++] = val;
+    }
+};
+
+benchmark.do(10000000, [
+    testForKeys,
+    testForIn,
+    testForOf,
+]);
